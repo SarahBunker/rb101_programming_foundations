0a4d5c9734396463ba2d49771eafec7ff6024520
SarahBunker <ChemEnCoder@gmail.com>
2021-04-24 18:25:24 +0000

    little bits I did at work today.

--- lesson_2/calculator_following_ex.rb
+++ lesson_2/calculator_following_ex.rb
@@ -20,12 +20,18 @@ number2 = gets.chomp
 puts "What operation would you like to perform? 1)add 2)subtract 3)multiply 4)divide"
 operation = gets.chomp
 
-if operator == "1"
-  results = number1.to_i + number2.to_i
-elsif operator == "2"
-  results = number1.to_i - number2.to_i
-elsif operator == "3"
-  results = number1.to_i * number2.to_i
+if operation == "1"
+  result = number1.to_i + number2.to_i
+elsif operation == "2"
+  result = number1.to_i - number2.to_i
+elsif operation == "3"
+  result = number1.to_i * number2.to_i
 else
-  results = number1.to_f / number2.to_f
-  
+  result = number1.to_f / number2.to_f
+end
+
+puts "The result is #{result}."
+
+=begin
+I was using add instead of 1
+=end
