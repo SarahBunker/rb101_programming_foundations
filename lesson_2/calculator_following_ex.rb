e7cd78ef4224a416a49f1466778b3ae94b8d0b76
SarahBunker <ChemEnCoder@gmail.com>
2021-04-24 15:51:21 +0000

    tired coding

--- lesson_2/calculator_following_ex.rb
+++ lesson_2/calculator_following_ex.rb
@@ -8,3 +8,24 @@ if loops do not create a block and so variables do not need to be initialized ou
 
 they did not convert to integers imediatly so that you could do float division
 =end
+
+puts "Welcome to Calculator"
+
+puts "What is the first number?"
+number1 = gets.chomp
+
+puts "What is the second number?"
+number2 = gets.chomp
+
+puts "What operation would you like to perform? 1)add 2)subtract 3)multiply 4)divide"
+operation = gets.chomp
+
+if operator == "1"
+  results = number1.to_i + number2.to_i
+elsif operator == "2"
+  results = number1.to_i - number2.to_i
+elsif operator == "3"
+  results = number1.to_i * number2.to_i
+else
+  results = number1.to_f / number2.to_f
+  
