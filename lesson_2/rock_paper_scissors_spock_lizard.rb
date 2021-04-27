5a9e1e6a47aa27a24db07d77ded210a3144694ba
SarahBunker <ChemEnCoder@gmail.com>
2021-04-27 23:47:34 +0000

    functioning rock paper scissors, I want to try moving the choices to a hash to reduce complexity

--- lesson_2/rock_paper_scissors_spock_lizard.rb
+++ lesson_2/rock_paper_scissors_spock_lizard.rb
@@ -1,36 +1,34 @@
 VALID_CHOICES = %w(rock paper scissors spock lizard)
-VALID_CHOICES_LETTER = %W(r p sc sp l)
+VALID_CHOICES_LETTER = %w(r p sc sp l)
 
-score_array = [0,0]
+score_array = [0, 0]
 def prompt(message)
-  puts ("=> #{message}")
+  puts("=> #{message}")
 end
 
 def win?(player1, player2)
-  (player1 == "rock" && ( player2 == "lizard" || player2 == "scissors") ) ||
-    (player1 == "paper" && ( player2 == "rock" || player2 == "spock") ) ||
-    (player1 == "scissors" && ( player2 == "lizard" || player2 == "paper") ) ||
-    (player1 == "spock" && ( player2 == "rock" || player2 == "scissors") ) ||
-    (player1 == "lizard" && ( player2 == "paper" || player2 == "spock") )
+  (player1 == "rock" && (player2 == "lizard" || player2 == "scissors")) ||
+    (player1 == "paper" && (player2 == "rock" || player2 == "spock")) ||
+    (player1 == "scissors" && (player2 == "lizard" || player2 == "paper")) ||
+    (player1 == "spock" && (player2 == "rock" || player2 == "scissors")) ||
+    (player1 == "lizard" && (player2 == "paper" || player2 == "spock"))
 end
 
-def display_results(choice,computer_choice)
-  if win?(choice,computer_choice)
+def display_results(choice, computer_choice)
+  if win?(choice, computer_choice)
     prompt("You win!")
-  elsif win?(computer_choice,choice)
+  elsif win?(computer_choice, choice)
     prompt("The computer won.")
   else
     prompt("It's a tie.")
   end
 end
 
-def update_results(choice,computer_choice)
-  if win?(choice,computer_choice)
-    score_array[0] += 1
-  elsif win?(computer_choice,choice)
-    score_array[1] += 1
-  else
-    prompt("It's a tie.")
+def update_results(choice, computer_choice, score)
+  if win?(choice, computer_choice)
+    score[0] += 1
+  elsif win?(computer_choice, choice)
+    score[1] += 1
   end
 end
 
@@ -40,7 +38,8 @@ loop do # match loop
   loop do # game loop
     loop do
       prompt("choose one: #{VALID_CHOICES.join(', ')}")
-      prompt("or choose from the shortened options: #{VALID_CHOICES_LETTER.join(', ')}")
+      prompt("or choose from the shortened options:
+        #{VALID_CHOICES_LETTER.join(', ')}")
       choice = gets.chomp
       if VALID_CHOICES_LETTER.include?(choice)
         case choice
@@ -54,19 +53,28 @@ loop do # match loop
       break if VALID_CHOICES.include?(choice)
       prompt("That's not a valid choice.")
     end
-    
+
     computer_choice = VALID_CHOICES.sample
-    
+
     prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-    
-    display_results(choice,computer_choice)
-    update_results(choice,computer_choice)
-    
+
+    display_results(choice, computer_choice)
+    update_results(choice, computer_choice, score_array)
+
     prompt("The score is player: #{score_array[0]} computer: #{score_array[1]}")
-    if score_array[0]
+
+    if score_array[0] == 5
+      prompt("You made it to 5 wins and won this match!")
+    elsif score_array[1] == 5
+      prompt("The computer made it to 5 wins and won this match.")
+    end
+    break if (score_array[0] == 5) || (score_array[1] == 5)
+
     prompt("Do you want to play another game?")
     answer = gets.chomp
     break unless answer.downcase.start_with?("y")
-    
   end
+  prompt("Do you want to start another match?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
 end
